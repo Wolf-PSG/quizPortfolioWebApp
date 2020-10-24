2e83e4efba56ec513d1a9e8d8a488afe0b22d81f
Parmjit Singh <psg@pop-os.localdomain>
2020-10-24 15:04:23 +0100

    play quiz now working --work on submiting value to database and allowing _redirects

--- src/components/playQuiz/playQuiz.component.jsx
+++ src/components/playQuiz/playQuiz.component.jsx
@@ -45,7 +45,7 @@ const PlayQuiz = () => {
 
   const handlePost = (e) => {
     e.preventDefault();
-    axios.post('https://quiz-maker-psg-api.herokuapp.com/api/v1/score/', post);
+    axios.post('https://quiz-maker-psg-api.herokuapp.com/api/v1/score', post);
     history.push({
       pathname: '/finishQuiz',
       state: {score},
